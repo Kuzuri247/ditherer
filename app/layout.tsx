2d3e19f45ec39123fad176ecb1263b357babde70
Kuzuri247 <mindsetmatters47@gmail.com>
2026-03-24 18:30:48 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
+import './globals.css'
+
+const _geist = Geist({ subsets: ["latin"] });
+const _geistMono = Geist_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'v0 App',
+  description: 'Created with v0',
+  generator: 'v0.app',
+  icons: {
+    icon: [
+      {
+        url: '/icon-light-32.png',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        url: '/icon-dark-32.png',
+        media: '(prefers-color-scheme: dark)',
+      },
+    ],
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className="font-mono antialiased">
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Component from '@/components/dither-shader-interactive'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Component />
+    </main>
+  )
+}
